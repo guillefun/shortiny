ff84f0f4b2f92f287e6ebcd2a1d30f9a4ea2878a
Guillermo Fernández-Campoamor Fernández <guillermofcampoamor@gmail.com>
2024-03-15 02:12:20 +0100

    Update font naming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </head>
-      <body className={`font-ubuntu_mono ${inter.variable} ${ubuntu_mono}`}>
+      <body className={`font-ubuntu-mono ${inter.variable} ${ubuntu_mono}`}>
         <TRPCReactProvider>
             <NavHeader />
               <main>
--- src/app/utils/fonts.js
+++ src/app/utils/fonts.js
@@ -4,7 +4,7 @@ import { Ubuntu_Mono } from "next/font/google";
 export const ubuntu_mono_init = Ubuntu_Mono({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-ubuntu_mono',
+  variable: '--font-ubuntu-mono',
   weight: ['400', '700']
 })
 
